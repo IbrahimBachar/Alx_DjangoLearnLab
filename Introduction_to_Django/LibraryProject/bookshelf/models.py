2f212aa14466f324dbaf4868c19907350d61425f
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-09-01 00:57:22 +0200

    Utilizing the Django Admin Interface

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
     
-    def list_filter(self):
+    def book_display(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
     
     
